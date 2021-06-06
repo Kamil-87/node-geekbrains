7e1fb63239d571a95c1b618541dc4174da4d16d5
kamil-87 <k.kamil87-87@yandex.ru>
2021-06-06 12:12:48 +0500

    fixed

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 /*
 Напишите программу, которая будет принимать на вход несколько аргументов: дату и время в формате «час-день-месяц-год».
 Задача программы — создавать для каждого аргумента таймер с обратным отсчётом: посекундный вывод в терминал состояния таймеров (сколько осталось).
-По истечении какого-либо таймера, вместо сообщения о том, сколько осталось, требуется показать сообщение о завершении его работы.
+
+* По истечении какого-либо таймера, вместо сообщения о том, сколько осталось, требуется показать сообщение о завершении его работы.
 Важно, чтобы работа программы основывалась на событиях.
  */
 
 const events = require('events')
-const myEmit = new events.EventEmitter()
+const eventEmitter = new events.EventEmitter()
 
-const myTime = '41-10-04-06-2021'
+const timersSettings = []
+const myTime1 = '32-11-06-06-2021'
+const myTime2 = '31-11-06-06-2021'
+
 
 function dateConversion(date) {
   const arr = date.split('-').reverse()
@@ -21,17 +25,12 @@ function diffTime(plannedTime) {
   return new Date(plannedTime).getTime() - Date.now()
 }
 
-function addToTimersSettings(date) {
-  return diffTime(dateConversion(date))
-}
+function outputOfQuantityOfTimers(timers) {
 
-function outputOfQuantityOfTimers(date) {
-  const timersSettings = []
-  timersSettings.push(addToTimersSettings(date))
   let timersCount = 0
 
-  for (let i = 0; i < timersSettings.length; i++) {
-    const timeout = timersSettings[i]
+  for (let i = 0; i < timers.length; i++) {
+    const timeout = timers[i]
     timersCount = ++timersCount
 
     setTimeout(() => {
@@ -48,10 +47,20 @@ function outputOfQuantityOfTimers(date) {
   }, 1000)
 }
 
-myEmit.on('some_event', function (date) {
-  outputOfQuantityOfTimers(date)
-})
+function addToTimersSettings(date) {
+  const timer = diffTime(dateConversion(date))
+  if(timer > 1000) {
+    timersSettings.push(timer)
+    console.log(timersSettings )
+  }
+}
 
-myEmit.emit('some_event', process.argv[2])
-myEmit.emit('some_event', process.argv[3])
-myEmit.emit('some_event', process.argv[4])
+eventEmitter.on('my_event', addToTimersSettings)
+// eventEmitter.on('error', (error) => console.log('Error:', error))
+
+for(let i = 2; i < process.argv.length; i++) {
+  console.log(process.argv[i])
+  eventEmitter.emit('my_event', process.argv[i])
+}
+outputOfQuantityOfTimers(timersSettings)
+
